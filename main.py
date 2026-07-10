7ffd13cee1f862a8fa9365110e63cf5490e22a2b
luanfaraujo <ferreiraaraujoluan@gmail.com>
2026-07-09 22:28:06 -0300

    add initial Sheets connection

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from dotenv import load_dotenv
 from google import genai
 from PIL import Image
 import logging
+import gspread
+import datetime as dt
+
+dt_now = dt.datetime.now()
+today = str(dt_now.date())
+
 
 # Set up logging config
 logging.basicConfig(
@@ -16,20 +22,35 @@ logging.info("Robot started")
 # Load environment variables
 load_dotenv()
 client = genai.Client()
+gc = gspread.service_account(filename="credentials.json")
 logging.info("Environment variables loaded and client initialized")
 
+# Load sheet
+gsheet = gc.open("Monthly expenses tracking")
+worksheet = gsheet.get_worksheet(0)
+
+# Load images
 for file in os.listdir("images_to_process"):
     if file.endswith(".jpg"):
         nf_to_process = os.path.join("images_to_process", file)
         img = Image.open(nf_to_process)
         logging.info("Image loaded successfully")
+        # implement delete the image after processing, but not now
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=[img, "De acordo com essa imagem de uma nota fiscal, me informe, apenas: o nome do item que eu comprei e o valor total de cada um."]
 )
 
-# Extract token usage metadata and output to log
+# Populate the Google Sheet with the extracted data
+# TODO need to make this work for every item in the response, loop it somehow
+col_a_values = worksheet.col_values(1) # find all rows w/ data in column A
+next_empty_row = len(col_a_values) + 1 # the next empty row is the length of filled rows + 1
+worksheet.update_acell(f"A{next_empty_row}", today)
+# worksheet.update_acell(f"C{next_empty_row}", response.text) # TODO write the response to the next empty row in column C
+# worksheet.update_acell(f"D{next_empty_row}", response.text) # TODO write the response to the next empty row in column D
+
+# Extract token usage metadata and output to log, for control
 usage = response.usage_metadata
 
 print(response.text)
